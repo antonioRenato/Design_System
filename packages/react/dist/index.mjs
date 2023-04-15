004214890e43f802f3562bd0390a83a10ab39302
antonioRenato <antoniorpfs@gmail.com>
2023-04-15 20:31:11 -0300

    used stiches

--- packages/react/dist/index.mjs
+++ packages/react/dist/index.mjs
@@ -18,6 +18,7 @@ var colors = {
 
 // src/index.tsx
 import { jsx } from "react/jsx-runtime";
+
 function App() {
   return /* @__PURE__ */ jsx("h1", { style: { color: colors.ignite300 }, children: "Hello World" });
 }
